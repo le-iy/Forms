8fcc4c64ebeb0507b3cb34b0dfbbe19134e7bf4c
Le.i <220001650+le-iy@users.noreply.github.com>
2026-09-14 17:47:45 +0800

    Update it0015.js

--- SUBJECTS/it0015.js
+++ SUBJECTS/it0015.js
@@ -8,7 +8,14 @@ const IT0015 = {
     color: "#ffbc59",
 
     categories: {
-        formatives: [],
+        formatives: [
+            {
+                title: "Formative 1",
+                description: "NETACAD",
+                link: "https://forms.cloud.microsoft/r/R1zz4BXfVq",
+                collabLink: "https://forms.cloud.microsoft/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EK1ypcd9WIhGl16-VZ5DfUxUMEZGWlRQRjlBQTdDR041UU85NUdZMDhLRC4u&Token=710f6b2b71ac4b498b4fb1cd66c82594"
+            }
+        ],
 
         summatives: [],
 
