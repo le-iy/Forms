4ae61eb9892ee41abdc5e909537f2a5b2a9020f8
Le.i <220001650+le-iy@users.noreply.github.com>
2026-09-14 17:56:32 +0800

    Update it0015.js

--- SUBJECTS/it0015.js
+++ SUBJECTS/it0015.js
@@ -14,7 +14,13 @@ const IT0015 = {
                 description: "NETACAD",
                 link: "https://forms.cloud.microsoft/r/R1zz4BXfVq",
                 collabLink: "https://forms.cloud.microsoft/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EK1ypcd9WIhGl16-VZ5DfUxUMEZGWlRQRjlBQTdDR041UU85NUdZMDhLRC4u&Token=710f6b2b71ac4b498b4fb1cd66c82594"
-            } 
+            },
+            {
+                title: "Module 2",
+                description: "NETACAD",
+                link: "https://forms.cloud.microsoft/r/zbmzXKUPRd",
+                collabLink: "https://forms.cloud.microsoft/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EK1ypcd9WIhGl16-VZ5DfUxURVpRTkVLUDNSRk1JUFVQV0lLOUcwMzJQMi4u&Token=50b5b1f0f9aa4a7facc54103ba51e9c7"
+            }
         ],
 
         summatives: [],
