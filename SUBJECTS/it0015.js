576495a7d3be48ae67e92d4db7f129b152bbd244
Le.i <220001650+le-iy@users.noreply.github.com>
2026-09-14 19:02:31 +0800

    Update it0015.js

--- SUBJECTS/it0015.js
+++ SUBJECTS/it0015.js
@@ -10,25 +10,25 @@ const IT0015 = {
     categories: {
         formatives: [
             {
-                title: "Module 1",
+                title: "CYU Module 1",
                 description: "NETACAD",
                 link: "https://forms.cloud.microsoft/r/R1zz4BXfVq",
                 collabLink: "https://forms.cloud.microsoft/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EK1ypcd9WIhGl16-VZ5DfUxUMEZGWlRQRjlBQTdDR041UU85NUdZMDhLRC4u&Token=710f6b2b71ac4b498b4fb1cd66c82594"
             },
             {
-                title: "Module 2",
+                title: "CYU Module 2",
                 description: "NETACAD",
                 link: "https://forms.cloud.microsoft/r/zbmzXKUPRd",
                 collabLink: "https://forms.cloud.microsoft/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EK1ypcd9WIhGl16-VZ5DfUxURVpRTkVLUDNSRk1JUFVQV0lLOUcwMzJQMi4u&Token=50b5b1f0f9aa4a7facc54103ba51e9c7"
             },
             {
-                title: "Module 3",
+                title: "CYU Module 3",
                 description: "NETACAD",
                 link: "https://forms.cloud.microsoft/r/RkJWBVaNe9",
                 collabLink: "https://forms.cloud.microsoft/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EK1ypcd9WIhGl16-VZ5DfUxUNzBNUzJNSk1NR1dISU81Qkc2Ulc5VjVTTS4u&Token=134aa9ac1e81453cb83cbdc81761a04e"
             },
             {
-                title: "Module 4",
+                title: "CYU Module 4",
                 description: "NETACAD",
                 link: "https://forms.cloud.microsoft/r/XhnhjX0mMS",
                 collabLink: "https://forms.cloud.microsoft/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EK1ypcd9WIhGl16-VZ5DfUxUNjRSWTdNREI5S1k2OVdaNzRQWDZFTkJLMy4u&Token=5b3e297d67764da888842631b05aa35d"
